e7d3a6147d88c61faa9e505f05740789bec4c1be
richard <coolwonk@gmail.com>
2017-12-17 10:45:15 -0800

    Added status topic, store total gallons in file.

--- FloMe.py
+++ FloMe.py
@@ -5,6 +5,9 @@ import requests
 import time
 import password
 
+global flowMeter1
+global flowMeter2
+
 '''This code subscribes to a mqtt broker that is delivering data from two
 flowmeters. The flowmeter data is in raw counts or 'ticks' from the
 hall effect sensors that detect the turning of a small magnetic turbine
@@ -16,7 +19,7 @@ APIKey = "xxxx"
 MYCHANNEL = "xxxx"
 '''
 # mqtt subscription topics
-subscriptions = [("yard/water/flow1", 0), ("yard/water/flow2", 0)]
+subscriptions = [("yard/water/flow1" , 0) , ("yard/water/flow2" , 0)]
 
 
 # The callback for when the client receives a CONNACK response from the server.
@@ -28,9 +31,9 @@ def on_connect(client, userdata, flags, rc):
         Connected = True  # Signal connection
         # Subscribing in on_connect() means that if we lose the connection and
         # reconnect then subscriptions will be renewed.
-        client.subscribe(subscriptions)
+        client.subscribe (subscriptions)
     else:
-        print("Connection failed" + str(rc))
+        print("Connection failed" + str (rc))
 
 
 # The callback for when a PUBLISH message is received from the server.
@@ -42,39 +45,47 @@ def on_message(client, userdata, msg):
     global flowMeter2
 
     topic = msg.topic
-    sys.stdout.write(topic + " ")
+    sys.stdout.write (topic + " ")
     if topic == 'yard/water/flow1':
-        flowMeter1.accumulate(msg)
+        flowMeter1.accumulate (msg)
     if topic == 'yard/water/flow2':
-        flowMeter2.accumulate(msg)
+        flowMeter2.accumulate (msg)
 
-    t = time.time()
+    t = time.time ()
     if t - lasttime > 15:  # report data to thingspeak max once per 30 sec
         dat = {'key': password.APIKey}
-        gallons = flowMeter1.send()
+        gallons = flowMeter1.send ()
         if gallons != -1:  # check for 'send nothing' signal
             total_gallons += gallons
             dat['field1'] = gallons
 
-        gallons = flowMeter2.send()
+        gallons = flowMeter2.send ()
         if gallons != -1:
             total_gallons += gallons
             dat['field2'] = gallons
 
-        # if there is some data, send it
+            # total gallons must be persistent
+        with open ("totalgallons", "w") as f:
+            f.write (str (total_gallons))
+
+            # if there is some data, send it
         if 'field1' in dat or 'field2' in dat:
             dat['field3'] = total_gallons
             print('sending')
             print(dat)
-            lasttime = time.time()
-            requests.post('https://api.thingspeak.com/update', data=dat)
+            lasttime = time.time ()
+            requests.post ('https://api.thingspeak.com/update' , data=dat)
+            # for debug, performance monitoring
+            client.publish ("status" , "flow posted")
 
     # reset total ticks (therefore gallons) once per month
-    day = time.localtime(t)[2]
-    hour = time.localtime(t)[3]
-    minute = time.localtime(t)[4]
+    day = time.localtime (t)[2]
+    hour = time.localtime (t)[3]
+    minute = time.localtime (t)[4]
     if day == 1 and hour == 1 and minute == 1:
         total_gallons = 0
+        with open ("totalgallons", "w") as f:
+            f.write (str (total_gallons))
 
 
 class TsDataManager:
@@ -88,11 +99,11 @@ class TsDataManager:
     def accumulate(self, mess):
         """accumulate flowmeter counts"""
         try:  # use parse library to extract flow meter ticks from message
-            tic = int(mess.payload.decode("utf-8"))
+            tic = int (mess.payload.decode ("utf-8"))
             print(tic)
             self.ticks += tic
         except  StandardError:
-             print('message decode error {}'.format(mess.payload.decode("utf-8")))
+            print('message decode error {}'.format (mess.payload.decode ("utf-8")))
 
     def send(self):
         """COMPUTE GALLONS
@@ -105,7 +116,7 @@ class TsDataManager:
             self.valve_on = True
             gallons = 0  # send start/stop marker
         elif self.valve_on == True and self.ticks == 0:  # valve has just turned off
-            # check to see if the valves are shutting off            
+            # check to see if the valves are shutting off
             self.valve_on = False
             gallons = 0  # send start/stop marker
         elif self.valve_on == False and self.ticks == 0:  # valve in steady state off
@@ -125,23 +136,28 @@ def main():
     global flowMeter1
     global flowMeter2
     print('Running FloMe')
-    total_gallons = 0
-    lasttime = time.time()
-    flowMeter1 = TsDataManager()
-    flowMeter2 = TsDataManager()
-    client = mqtt.Client()
+    try:
+        with open ("totalgallons", "r") as f:
+            total_gallons = float(f.readlines()[0])
+    except (OSError , IOError) as e:
+        total_gallons = 0
+
+    lasttime = time.time ()
+    flowMeter1 = TsDataManager ()
+    flowMeter2 = TsDataManager ()
+    client = mqtt.Client ()
     client.on_connect = on_connect
     client.on_message = on_message
-    client.connect("raspberrypi.local")
-    client.loop_start()  # start the loop
+    client.connect ("raspberrypi.local")
+    client.loop_start ()  # start the loop
     while Connected != True:  # Wait for connection
-        time.sleep(0.1)
-        sys.stdout.write('.')
-        sys.stdout.flush()
+        time.sleep (0.1)
+        sys.stdout.write ('.')
+        sys.stdout.flush ()
     # Blocking call that processes network traffic, dispatches callbacks and
     # handles reconnecting.
-    client.loop_forever()
+    client.loop_forever ()
 
 
 if __name__ == "__main__":
-    main()
+    main ()
